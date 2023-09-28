7912ab345e27d3d11671fab59c602de0c6b89265
Dani Ocaranza <docaranzan@gmail.com>
2023-09-28 19:56:41 -0300

    code refactoring, added operations and debug utils
    changed data values for numbers, added helper functions for
    operations, created all operation functions and added code
    to aid in debugging.

--- js/main.js
+++ js/main.js
@@ -1,58 +1,96 @@
+const DEBUG = false;
+
 const calc = {
-  displayMain: '',
+  numInput: '',
   operator: '',
   result: '',
+  lastOp: '',
+  operand: {
+    left: 0,
+    right: 0
+  },
   readInput: function(e) {
     let key = e.target.dataset.key;
-    if (key[0] === 'K') {
-      calc.displayMain += key[1];
+    
+    if (Number(key)) {
+      this.numInput += key;
       return;
     }
+
     if (key === 'cls') {
-        if (this.displayMain || this.result || this.operator){
-          this.clearAll();
-          return;
-        }
+      this.clearAll();
+      return;
     }
+
     if (key === 'enter'){
-        if (this.operator) {
-          this.operate(this.operator);
-          return;
-        }
+      if (this.operator) {
+        this.operate(this.operator);
+        return;
+      }
     }
     
     if (this.operator === '') {
       this.operator = key;
-      this.result = this.displayMain;
-      this.displayMain = '';
+      this.result = this.numInput;
+      this.numInput = '';
       return;
     }
 
-    this.operate(this.operator)
   },
   operate: function(operator) {
+    this.setOperands();
     switch (operator) {
       case 'add':
-        calc.add()
+        calc.add();
+        break;
+      case 'sub':
+        calc.sub();
+        break;
+      case 'mult':
+        calc.mult();
+        break;
+      case 'div':
+        calc.div();
+        break;
     }
   },
   add: function(){
-    let num1 = Number(calc.displayMain);
-    let num2 = Number(calc.result);
-    let result = num1 + num2;
-    calc.result = String(result);
+    this.setResult(this.operand.left + this.operand.right);
+  },
+  sub: function(){
+    this.setResult(this.operand.left - this.operand.right);
+  },
+  mult: function(){
+    this.setResult(this.operand.left * this.operand.right);
+  },
+  div: function(){
+    this.setResult(this.operand.left / this.operand.right);
+  },
+  setOperands: function() {
+    this.operand.left = Number(this.result);
+    this.operand.right = Number(this.numInput);
+  },
+  setResult: function(number) {
+    this.result = String(number);
   },
   updateDisplay: function() {
     let main = document.querySelector('.num-main');
     let result = document.querySelector('.num-result');
-    main.textContent = this.displayMain;
+    main.textContent = this.numInput;
     result.textContent = this.result;
   },
   clearAll: function() {
-    this.displayMain = '';
+    this.numInput = '';
     this.operator = '';
     this.result = '';
-  } 
+    this.lastOp = '';
+  },
+  logMe: function() {
+    log('numInput:', this.numInput);
+    log('operator:', this.operator);
+    log('result:', this.result);
+    log('lastOp:', this.lastOp);
+  }
 }
 
 let keys = document.querySelectorAll('.key')
@@ -65,3 +103,13 @@ keys.forEach((key) => {
   
 });
 
+function log(message, ...args){
+  if (DEBUG) {
+    if (args) {
+      args.forEach((arg) => {
+        message += ` ${arg}\n`;
+      });
+    }
+    console.log(message);
+  }
+}
