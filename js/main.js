698410008dfdc9abcb9bdb97b581877789915c0d
Dani Ocaranza <docaranzan@gmail.com>
2023-09-28 13:39:54 -0300

    Refactored code. Created clear and add ops

--- js/main.js
+++ js/main.js
@@ -1,20 +1,67 @@
-
 const calc = {
-  displayMain: document.querySelector('.num-main'),
+  displayMain: '',
+  operator: '',
+  result: '',
   readInput: function(e) {
-    let key = e.target;
-    if (key.dataset.key[0] === 'K') {
-      calc.printNum(key.dataset.key[1]);
+    let key = e.target.dataset.key;
+    if (key[0] === 'K') {
+      calc.displayMain += key[1];
+      return;
+    }
+    if (key === 'cls') {
+        if (this.displayMain || this.result || this.operator){
+          this.clearAll();
+          return;
+        }
+    }
+    if (key === 'enter'){
+        if (this.operator) {
+          this.operate(this.operator);
+          return;
+        }
+    }
+    
+    if (this.operator === '') {
+      this.operator = key;
+      this.result = this.displayMain;
+      this.displayMain = '';
+      return;
+    }
+
+    this.operate(this.operator)
+  },
+  operate: function(operator) {
+    switch (operator) {
+      case 'add':
+        calc.add()
     }
   },
-  printNum: function(num) {
-    this.displayMain.textContent += num;
-  }
+  add: function(){
+    let num1 = Number(calc.displayMain);
+    let num2 = Number(calc.result);
+    let result = num1 + num2;
+    calc.result = String(result);
+  },
+  updateDisplay: function() {
+    let main = document.querySelector('.num-main');
+    let result = document.querySelector('.num-result');
+    main.textContent = this.displayMain;
+    result.textContent = this.result;
+  },
+  clearAll: function() {
+    this.displayMain = '';
+    this.operator = '';
+    this.result = '';
+  } 
 }
 
 let keys = document.querySelectorAll('.key')
 
 keys.forEach((key) => {
-  key.addEventListener('click',calc.readInput);
+  key.addEventListener('click', (e) => {
+    calc.readInput(e)
+    calc.updateDisplay()
+  });
+  
 });
 
